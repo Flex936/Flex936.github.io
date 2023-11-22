6e0358156b0c7d386ee3cf12e285b47997c374fd
Tancsik Alex <tancsik.alex@gmail.com>
2023-11-22 17:51:24 +0100

    organising folders and uploading the reamining websites

---
+++ Pancake/script.js
@@ -0,0 +1,27 @@
+function Home() {
+    document.getElementById('Home').style.display="block"
+    document.getElementById('Receipt').style.display="none"
+    document.getElementById('TheAmericanWay').style.display="none"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Receipt() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('Receipt').style.display="block"
+    document.getElementById('TheAmericanWay').style.display="none"
+    document.getElementById('Activities').style.display="none"
+}
+
+function TheAmericanWay() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('Receipt').style.display="none"
+    document.getElementById('TheAmericanWay').style.display="block"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Activities() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('Receipt').style.display="none"
+    document.getElementById('TheAmericanWay').style.display="none"
+    document.getElementById('Activities').style.display="block"
+}
