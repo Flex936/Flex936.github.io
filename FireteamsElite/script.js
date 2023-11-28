f5f3509e83a8eb08dadf83dc713fbe37e33cc837
Tekó Olimpia <olimpia@rakoczi.lo>
2023-11-28 12:01:30 +0100

    alien web

---
+++ FireteamsElite/script.js
@@ -0,0 +1,27 @@
+function Home() {
+    document.getElementById('Home').style.display="block"
+    document.getElementById('PathogenDLC').style.display="none"
+    document.getElementById('Steam').style.display="none"
+    document.getElementById('Activities').style.display="none"
+}
+
+function PathogenDLC() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('PathogenDLC').style.display="block"
+    document.getElementById('Steam').style.display="none"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Steam() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('PathogenDLC').style.display="none"
+    document.getElementById('Steam').style.display="block"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Activities() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('PathogenDLC').style.display="none"
+    document.getElementById('Steam').style.display="none"
+    document.getElementById('Activities').style.display="block"
+}
