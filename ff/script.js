768a800bcb187b16a08d56afa52098a5aa79d077
Tancsik Alex <tancsik.alex@gmail.com>
2024-07-19 00:09:07 +0200

    placement and code update

--- ff/script.js
+++ ff/script.js
@@ -1,57 +1,23 @@
-const PornHub = document.getElementById('PornHub')
-const Artists = document.getElementById("Artists")
-const HanimeTV = document.getElementById("HanimeTV")
-const PornHubB = document.getElementById('PHB')
-const ArtistsB = document.getElementById("ASB")
-const HanimeTVB = document.getElementById("HTVB")
+const listSections=[document.getElementById('PornHub'),document.getElementById("Artists"), document.getElementById("HanimeTV")]
+const listButton=[document.getElementById('PHB'),document.getElementById("ASB"), document.getElementById("HTVB")]
 
-const listButton = [document.getElementById('HanimeTV')]
-
-function colorChange() {
-    for (let i=0; i < listButton.length; i++){
-        if (listButton[i] != document.getElementById(this)){
+function buttonClick(clicked_id) {
+    for (let i=0; i < listButton.length;i++){
+        if (listButton[i] != clicked_id){
             listButton[i].style.backgroundColor="rgb(20, 20, 20)"
-            listButton[i].style.backgroundColor = "orange"
+            listButton[i].style.color = "orange"
         }
         else{
             listButton[i].style.backgroundColor="orange"
-            listButton[i].style.backgroundColor = "rgb(20, 20, 20)"
+            listButton[i].style.color = "rgb(20, 20, 20)"
         }
     }
-}
-
-function PH(){
-    PornHub.style.display="grid"
-    Artists.style.display="none"
-    HanimeTV.style.display = "none"
-    PornHubB.style.backgroundColor = "orange"
-    PornHubB.style.color = "rgb(20, 20, 20)"
-    ArtistsB.style.backgroundColor = "rgb(20, 20, 20)"
-    ArtistsB.style.color = "orange"
-    HanimeTVB.style.backgroundColor = "rgb(20, 20, 20)"
-    HanimeTVB.style.color = "orange"
-}
-
-function AS(){
-    PornHub.style.display = "none"
-    Artists.style.display = "grid"
-    HanimeTV.style.display = "none"
-    PornHubB.style.backgroundColor = "rgb(20, 20, 20)"
-    PornHubB.style.color = "orange"
-    ArtistsB.style.backgroundColor = "orange"
-    ArtistsB.style.color = "rgb(20, 20, 20)"
-    HanimeTVB.style.backgroundColor = "rgb(20, 20, 20)"
-    HanimeTVB.style.color = "orange"
-}
-
-function HTV(){
-    PornHub.style.display = "none"
-    Artists.style.display = "none"
-    HanimeTV.style.display = "grid"
-    PornHubB.style.backgroundColor = "rgb(20, 20, 20)"
-    PornHubB.style.color = "orange"
-    ArtistsB.style.backgroundColor = "rgb(20, 20, 20)"
-    ArtistsB.style.color = "orange"
-    HanimeTVB.style.backgroundColor = "orange"
-    HanimeTVB.style.color = "rgb(20, 20, 20)"
+    for (let i=0; i < listSections.length;i++){
+        if (listSections[i].id[0] != clicked_id.id[0]){
+            listSections[i].style.display="none"
+        }
+        else{
+            listSections[i].style.display="grid"
+        }
+    }
 }
