6e918e3ca5c7ad0723d691f8864cac19c428a4f4
Tancsik Alex <tancsik.alex@gmail.com>
2024-07-24 23:58:50 +0200

    Sites update

--- ff/script.js
+++ ff/script.js
@@ -1,22 +1,16 @@
-const listSections=[document.getElementById('PornHub'),document.getElementById("Artists"), document.getElementById("HanimeTV")]
-const listButton=[document.getElementById('PHB'),document.getElementById("ASB"), document.getElementById("HTVB")]
+const listSections=[document.getElementById('PornHub'),document.getElementById("Artists"), document.getElementById("HanimeTV"), document.getElementById("Sites")]
+const listButton=[document.getElementById('PHB'),document.getElementById("ASB"), document.getElementById("HTVB"), document.getElementById("SB")]
 
 function buttonClick(clicked_id) {
     for (let i=0; i < listButton.length;i++){
-        if (listButton[i] != clicked_id){
+        if (listSections[i].id[0] != clicked_id.id[0]){
             listButton[i].style.backgroundColor="rgb(20, 20, 20)"
             listButton[i].style.color = "orange"
+            listSections[i].style.display="none"
         }
         else{
             listButton[i].style.backgroundColor="orange"
             listButton[i].style.color = "rgb(20, 20, 20)"
-        }
-    }
-    for (let i=0; i < listSections.length;i++){
-        if (listSections[i].id[0] != clicked_id.id[0]){
-            listSections[i].style.display="none"
-        }
-        else{
             listSections[i].style.display="grid"
         }
     }
