d9aadcf27e9160fbea0f69cee52562fb1fd1b64f
Tancsik Alex <tancsik.alex@gmail.com>
2024-07-29 22:43:12 +0200

    Design and list update

--- ff/script.js
+++ ff/script.js
@@ -2,27 +2,25 @@ for (let k=1; k < 635;k++){
     var pics = document.createElement("img")
     try{
         pics.src='artists/thiccwithaq/'+k+'.jpg'
-        document.getElementById("Thiccwithaq").appendChild(pics)
+        document.getElementById('Thiccwithaq').appendChild(pics)
     }catch(gifError){
         pics.src='artists/thiccwithaq/'+k+'.gif'
-        document.getElementById("Thiccwithaq").appendChild(pics)
+        document.getElementById('Thiccwithaq').appendChild(pics)
     }
 }
 
-const listSections=[document.getElementById('PornHub'),document.getElementById("Leaks"),document.getElementById("HanimeTV"),document.getElementById("Artists"),document.getElementById("Sites"),document.getElementById('Thiccwithaq')]
-const listButton=[document.getElementById('PHB'),document.getElementById("LSB"),document.getElementById("HTVB"),document.getElementById("ASB"),document.getElementById("SSB")]
+const listSections=[document.getElementById('PornHub'),document.getElementById('Leaks'),document.getElementById('HanimeTV'),document.getElementById('Artists'),document.getElementById('Sites'),document.getElementById('Thiccwithaq')]
+const listButton=[document.getElementById('PHB'),document.getElementById('LSB'),document.getElementById('HTVB'),document.getElementById('ASB'),document.getElementById('SSB')]
+const hrc = document.getElementById('hri')  
 
 function buttonClick(clicked_id) {
     for (let i=0; i < listSections.length;i++){
         if (listSections[i].id[0] != clicked_id.id[0]){
-            listSections[i].style.display="none"
-            listButton[i].style.backgroundColor="rgb(20, 20, 20)"
-            listButton[i].style.color = "orange"
+            listSections[i].style.display='none'
         }
         else{
-            listButton[i].style.backgroundColor="orange"
-            listButton[i].style.color = "rgb(20, 20, 20)"
-            listSections[i].style.display="grid"
+            listSections[i].style.display='grid'
+            hrc.style.marginLeft=i+'00%'
         }
     }
 }
