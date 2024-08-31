78bc788d78efa4bf9383001c4622407034118d4a
Tancsik Alex <tancsik.alex@gmail.com>
2024-08-31 17:12:51 +0200

    new features and more schedules

--- teko/script.js
+++ teko/script.js
@@ -1,10 +1,35 @@
 function details(menu){
     if (menu.style.width != '110%'){
         menu.style.cssText="width: 110%; height: auto; transform: translateX(-6.5%); border-width: 3px; z-index: 2;"
-        menu.children[0].style.display='flex'
+        menu.querySelector('table').style.display='flex'
     }
     else{
         menu.style.cssText="width: 70%; height: 0; transform: translateX(0); border-width: 6px; z-index: 1;"
-        menu.children[0].style.display='none'
+        menu.querySelector('table').style.display='none'
     }
+}
+
+function listClasses(table){
+    let list = table.querySelectorAll('td')
+    if(table.classList.contains('active')){
+        table.classList.remove('active')
+        for (let i=1; i < list.length; i++){
+            list[i].style.display='none'
+        }
+    }
+    else{
+        table.classList.add('active')
+        for (let i=1; i < list.length; i++){
+            list[i].style.display='table-cell'
+        }
+    }
+}
+
+function switchSchedule(thisClass){
+    let list = document.getElementsByClassName('schedule')
+    for (let i=0; i < list.length; i++){
+        list[i].style.display='none'
+    }
+    document.getElementById(thisClass).style.display='table-row-group'
+    document.getElementById('class').innerHTML=thisClass
 }
