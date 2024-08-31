0c9d03ab468586db355ec71169bc0df1464bbc2a
Tancsik Alex <tancsik.alex@gmail.com>
2024-08-31 09:02:04 +0200

    code cleanup

--- teko/script.js
+++ teko/script.js
@@ -1,10 +1,10 @@
-function details(slider){
-    if (slider.style.width != '110%'){
-        slider.style.cssText="width: 110%; height: auto; transform: translateX(-6.5%); border-width: 3px; z-index: 2;"
-        slider.children[0].style.display='flex'
+function details(menu){
+    if (menu.style.width != '110%'){
+        menu.style.cssText="width: 110%; height: auto; transform: translateX(-6.5%); border-width: 3px; z-index: 2;"
+        menu.children[0].style.display='flex'
     }
     else{
-        slider.style.cssText="width: 70%; height: 0; transform: translateX(0); border-width: 6px; z-index: 1;"
-        slider.children[0].style.display='none'
+        menu.style.cssText="width: 70%; height: 0; transform: translateX(0); border-width: 6px; z-index: 1;"
+        menu.children[0].style.display='none'
     }
 }
