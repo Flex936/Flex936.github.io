6adb631f40ee3d85a91769b7214309caf4199af5
Tancsik Alex <tancsikalex@teko.hu>
2024-10-02 14:10:33 +0200

    dark mode & deletion of old website

--- teko/script.js
+++ teko/script.js
@@ -1,35 +1,35 @@
 function details(menu) {
-  if (menu.style.width != '110%') {
+  if (menu.style.width != "110%") {
     menu.style.cssText =
-      'width: 110%; height: auto; transform: translateX(-6.5%); border-width: 3px; z-index: 2;';
-    menu.querySelector('table').style.display = 'flex';
+      "width: 110%; height: auto; transform: translateX(-6.5%); border-width: 3px; z-index: 2;";
+    menu.querySelector("table").style.display = "flex";
   } else {
     menu.style.cssText =
-      'width: 70%; height: 0; transform: translateX(0); border-width: 6px; z-index: 1;';
-    menu.querySelector('table').style.display = 'none';
+      "width: 70%; height: 0; transform: translateX(0); border-width: 6px; z-index: 1;";
+    menu.querySelector("table").style.display = "none";
   }
 }
 
 function listClasses(table) {
-  let list = table.querySelectorAll('td');
-  if (table.classList.contains('active')) {
-    table.classList.remove('active');
+  let list = table.querySelectorAll("td");
+  if (table.classList.contains("active")) {
+    table.classList.remove("active");
     for (let i = 1; i < list.length; i++) {
-      list[i].style.display = 'none';
+      list[i].style.display = "none";
     }
   } else {
-    table.classList.add('active');
+    table.classList.add("active");
     for (let i = 1; i < list.length; i++) {
-      list[i].style.display = 'table-cell';
+      list[i].style.display = "table-cell";
     }
   }
 }
 
 function switchSchedule(thisClass) {
-  let list = document.getElementsByClassName('schedule');
+  let list = document.getElementsByClassName("schedule");
   for (let i = 0; i < list.length; i++) {
-    list[i].style.display = 'none';
+    list[i].style.display = "none";
   }
-  document.getElementById(thisClass).style.display = 'table-row-group';
-  document.getElementById('class').innerHTML = thisClass;
+  document.getElementById(thisClass).style.display = "table-row-group";
+  document.getElementById("class").innerHTML = thisClass;
 }
