632735d580806c763945d8c57ce96a8dfa8d7c7b
Tancsik Alex <tancsik.alex@gmail.com>
2024-10-06 20:21:12 +0200

    Css changes

--- teko/script.js
+++ teko/script.js
@@ -1,11 +1,11 @@
 function details(menu) {
   if (menu.style.width != "110%") {
     menu.style.cssText =
-      "width: 110%; height: auto; transform: translateX(-6.5%); border-width: 3px; z-index: 2;";
+      "width: 110%; height: auto; transform: translateX(-6.5%); z-index: 2;";
     menu.querySelector("table").style.display = "flex";
   } else {
     menu.style.cssText =
-      "width: 70%; height: 0; transform: translateX(0); border-width: 6px; z-index: 1;";
+      "width: 70%; height: 0; transform: translateX(0); z-index: 1;";
     menu.querySelector("table").style.display = "none";
   }
 }
